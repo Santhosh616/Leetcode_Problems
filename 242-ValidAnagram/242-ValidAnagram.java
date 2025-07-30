35ae30513cc03b2e2fa0082bed3f4aa7de3b4aa4
SANTHOSH V S <151340441+Santhosh616@users.noreply.github.com>
2025-07-30 09:21:29 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,12 @@
+// Last updated: 7/30/2025, 9:21:25 AM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
