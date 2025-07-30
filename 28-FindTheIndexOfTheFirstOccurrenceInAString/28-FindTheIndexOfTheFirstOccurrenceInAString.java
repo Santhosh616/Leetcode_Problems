78441d0e88d304a7e71d412f441d245465bb0aa8
SANTHOSH V S <151340441+Santhosh616@users.noreply.github.com>
2025-07-30 09:31:29 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,11 @@
+// Last updated: 7/30/2025, 9:31:25 AM
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
